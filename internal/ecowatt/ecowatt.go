e9128cbb8df434aeaba67d8ee35f496f0b16a246
Pierre Kuhner <pkuhner@users.noreply.github.com>
2022-10-04 21:08:43 +0200

    feat: add error handling and synchronization

--- internal/ecowatt/ecowatt.go
+++ internal/ecowatt/ecowatt.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"sort"
+	"sync"
 	"time"
 )
 
@@ -15,12 +16,13 @@ type Ecowatt struct {
 	Client              *http.Client
 	BaseURL             *url.URL
 	BearerToken         BearerToken
-	Ratelimit           int
 	BearerTokenEndpoint *url.URL
 	BearerTokenLifetime int
+	Ratelimit           int
 	AuthorizationToken  string
 	LastcallTime        *time.Time
 	Signals             *Signals
+	mx                  sync.RWMutex
 }
 
 type BearerToken struct {
@@ -53,21 +55,22 @@ func New(baseURL *url.URL, ratelimit int, bearerTokenEndpoint *url.URL, bearerTo
 		Client:              &http.Client{},
 		BaseURL:             baseURL,
 		Ratelimit:           ratelimit,
+		AuthorizationToken:  authorizationToken,
 		BearerTokenEndpoint: bearerTokenEndpoint,
 		BearerTokenLifetime: bearerTokenLifetime,
-		AuthorizationToken:  authorizationToken,
 	}
 }
 
 func (ecw *Ecowatt) Start() {
-	bearerTokenAge := &time.Time{}
+	bearerTokenAge := time.Time{}
 
 	for {
 		log.Print("Updating signals...")
-		if int(time.Since(*bearerTokenAge)/time.Second) > ecw.BearerTokenLifetime {
+		if int(time.Since(bearerTokenAge)/time.Second) > ecw.BearerTokenLifetime {
 			log.Print("Retrieving a new bearer token...")
 			var tokenErr error
-			bearerTokenAge, tokenErr = ecw.GetBearerToken()
+			tokenErr = ecw.getBearerToken()
+			bearerTokenAge = time.Now()
 			if tokenErr != nil {
 				log.Printf("Couldn't retrieve a bearer token: %v", tokenErr)
 				time.Sleep(time.Duration(ecw.Ratelimit+1) * time.Second)
@@ -78,7 +81,7 @@ func (ecw *Ecowatt) Start() {
 			log.Print("\tBearer token is still valid...")
 		}
 
-		signalsErr := ecw.GetSignals()
+		signalsErr := ecw.updateSignals()
 		if signalsErr != nil {
 			log.Printf("\tCouldn't update signals: %v", signalsErr)
 		}
@@ -88,34 +91,41 @@ func (ecw *Ecowatt) Start() {
 	}
 }
 
-func (ecw *Ecowatt) GetBearerToken() (*time.Time, error) {
+func (ecw *Ecowatt) GetSignals() (*Signals, error) {
+	return ecw.getSignals()
+}
+
+func (ecw *Ecowatt) GetSignal(day int) (*Signal, error) {
+	return ecw.getSignalForDay(day)
+}
+
+func (ecw *Ecowatt) getBearerToken() error {
 	var result BearerToken
 
 	req, err := http.NewRequest("GET", ecw.BearerTokenEndpoint.String(), nil)
 	req.Header.Add("Authorization", "Basic "+ecw.AuthorizationToken)
 	resp, err := ecw.Client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't GET bearer token endpoint: %w", err)
+		return fmt.Errorf("Couldn't GET bearer token endpoint: %w", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't read response: %w", err)
+		return fmt.Errorf("Couldn't read response: %w", err)
 	}
 
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't unmarshal body: %w", err)
+		return fmt.Errorf("Couldn't unmarshal body: %w", err)
 	}
 
 	ecw.BearerToken = result
-	now := time.Now()
 
-	return &now, nil
+	return nil
 }
 
-func (ecw *Ecowatt) GetSignals() error {
+func (ecw *Ecowatt) updateSignals() error {
 	var result SignalsResponse
 
 	req, err := http.NewRequest("GET", ecw.BaseURL.JoinPath("/signals").String(), nil)
@@ -151,11 +161,27 @@ func (ecw *Ecowatt) GetSignals() error {
 		return signals[i].Jour.Before(signals[j].Jour)
 	})
 
+	ecw.mx.Lock()
 	ecw.Signals = &signals
+	ecw.mx.Unlock()
 
 	return nil
 }
 
-func (ecw *Ecowatt) SignalForDay(day int) (*Signal, error) {
-	return (*ecw.Signals)[day], nil
+func (ecw *Ecowatt) getSignals() (*Signals, error) {
+	ecw.mx.RLock()
+	defer ecw.mx.RUnlock()
+
+	return ecw.Signals, nil
+}
+
+func (ecw *Ecowatt) getSignalForDay(day int) (*Signal, error) {
+	ecw.mx.RLock()
+	if !(day >= 0 && day <= len(*ecw.Signals)) {
+		return nil, fmt.Errorf("Day %d does not exist in signals", day)
+	}
+	signal := (*ecw.Signals)[day]
+	ecw.mx.RUnlock()
+
+	return signal, nil
 }
